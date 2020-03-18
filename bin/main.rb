2700772bc843e5cfd8f1f4857fb785126668af3a
eduardo <eduardoreisalvarenga@gmail.com>
2020-03-18 20:31:05 -0300

    Adding jokes class

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 require 'telegram/bot'
+require_relative '../lib/jokes.rb'
 require_relative '../lib/daily_quotes.rb'
 token = '1142941945:AAEJgRnZ9KTwspfSLmNpzu2waocJH1sji3M'
 
@@ -10,7 +11,14 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "#{quote}\n\t-Author: #{author}")
     when '/joke'
       bot.api.send_message(chat_id: message.chat.id, text: "What's your name?")
-      bot.api.send_message(chat_id: message.chat.id, text: "This is a joke with your name")
+      bot.listen do |name|
+        if name
+          bot.api.send_message(chat_id: name.chat.id, text: name)
+          @joke = Jokes.new.random_joke(name.text)
+          break
+        end
+      end
+      bot.api.send_message(chat_id: message.chat.id, text: "#{@joke}")
     end
   end
 end
---
+++ lib/jokes.rb
@@ -0,0 +1,13 @@
+require 'rest-client'
+require 'json'
+
+class Jokes
+  def random_joke(name)
+    url = 'https://api.chucknorris.io/jokes/random'
+    resp = RestClient.get url
+    joke = JSON.parse(resp.body)["value"]
+    joke = joke.gsub("Chuck Norris", name)
+    random_joke(name) if !joke.include?(name) or joke.include? "Chuck Norris"
+    return joke.to_s
+  end
+end
