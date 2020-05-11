6fb3e8ebb88ba38a373c7a85062eee29f61bf3f8
Eduardo <eduardoreisalvarenga@gmail.com>
2020-05-11 16:53:13 -0300

    Fixing linter errors

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,6 +3,6 @@ require pp
 
 describe Main do
   it 'starts my program' do
-    expect(Main.new.main_method("jashdsiahd")).to raise_error
+    expect(Main.new.main_method('jashdsiahd')).to raise_error
   end
 end
